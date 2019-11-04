111a39ca00f8ba981a111a0ac1eb0984aa870f2d
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-11-04 02:06:18 -0600

    Updates

---
+++ precache-manifest.1a88deaf87956d19bad69796ac79f493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22243dee98cc267584632e97f6e22408",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "9321f3118f1fcb698381",
+    "url": "/Portafolio/static/css/main.1699ba80.chunk.css"
+  },
+  {
+    "revision": "b5ccd4dab8d964f6d5ef",
+    "url": "/Portafolio/static/js/2.3310a0bf.chunk.js"
+  },
+  {
+    "revision": "9321f3118f1fcb698381",
+    "url": "/Portafolio/static/js/main.d0b985f0.chunk.js"
+  },
+  {
+    "revision": "743e32ef4bc99d5ca8bd",
+    "url": "/Portafolio/static/js/runtime-main.4daf7d2c.js"
+  },
+  {
+    "revision": "1c86ad47a526b6dffc013d1f0eb58a01",
+    "url": "/Portafolio/static/media/earth-1149733_1920.1c86ad47.jpg"
+  }
+]);
