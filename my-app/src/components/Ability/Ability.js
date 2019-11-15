a9483fefbd4201a4cce148663699c19d148e973c
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-11-14 18:36:28 -0600

    se agrego seccion de habilidades

--- my-app/src/components/Ability/Ability.js
+++ my-app/src/components/Ability/Ability.js
@@ -6,7 +6,7 @@ import './Ability.css';
 // import ReactImage from './img/react-icon-png-7.png'
 // import FirebaseImage from './img/firebase.png'
 // import NpmImage from './img/npm-512.png'
-// import NodeImage from './img/nodejs.png'
+// import NodeImage from './img/nodejs.png' 
 // import JestImage from './img/jest-logo.png'
 
 
@@ -19,27 +19,32 @@ class Aptitudes extends React.Component{
     render(){
         return(
             <section className="section-ability">
-                <p className="text-ability">Estas son las aptitudes <br></br>con las que cuento</p>
+                <p className="text-ability">Aptitudes</p>
 
                 <div className="images-ability">
-                <div>
+                <div className = "soft">
                     <ul className="list-ability">
                         <li>Habilidades blandas:</li>
+                        <hr className="line-soft"></hr>
                         <br></br>
                         <li>- Autoaprendizaje</li>
                         <li>- Dar y recibir feedback</li>
                         <li>- Trabajo coolaborativo</li>
                         <li>- Autogestión</li>
                         <li>- Asertividad efectiva</li>
-                        <li>- comunicación</li>  
+                        <li>- Comunicación</li>
+                        <li>- Curiosidad</li>
+                        <li>- Proactividad</li>
+                        <li>- Y mucho más...</li>
                     </ul>
                     </div>
                     <div>
                         <img src={Avatar} alt="Github" className="avatar"></img>
                     </div>
-                    <div>
+                    <div className="tec">
                     <ul className="list-ability">
                         <li>Habilidades técnicas:</li>
+                        <hr className="line-soft"></hr>
                         <br></br>
                         <li>-Github</li>
                         <li>-Git</li>
