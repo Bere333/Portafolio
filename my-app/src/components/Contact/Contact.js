10a0c1e40189ec836400e9cea12862b5cd0bbe6c
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-11-17 22:00:58 -0600

    se redacto correctamente texto de la seccion de proyectos

--- my-app/src/components/Contact/Contact.js
+++ my-app/src/components/Contact/Contact.js
@@ -11,14 +11,14 @@ class Contact extends React.Component{
             <section className="section-contact">
                
                    <div className="made">
-                       <p className="text-made">Hecho con <i class="fas fa-heart"></i>, <i class="fas fa-atom fa-spin"></i> y <i class="fas fa-code"></i></p>
+                       <p className="text-made">Hecho con <i class="fas fa-heart" title="Amor"></i>, <i class="fas fa-atom fa-spin" title="React"></i> y <i class="fas fa-code" title="Código"></i></p>
                    </div>
 
                    <div className="box-contact">
                     <p className="icons-contact" >
-                    <a href="https://www.linkedin.com/in/berenice-ramos-6a5b39156/"><i className="fab fa-linkedin"></i></a>
-                    <a href="https://github.com/Bere333"><i class="fab fa-github-square"></i></a>
-                    <a href="https://www.linkedin.com/in/berenice-ramos-6a5b39156/"><i className="fas fa-envelope-square"></i></a>
+                    <a href="https://www.linkedin.com/in/berenice-ramos-6a5b39156/"><i className="fab fa-linkedin" title="LinkedIn"></i></a>
+                    <a href="https://github.com/Bere333"><i class="fab fa-github-square" title="Github"></i></a>
+                    <a href="https://www.linkedin.com/in/berenice-ramos-6a5b39156/"><i className="fas fa-envelope-square" title="Correo"></i></a>
                        
  
                     </p>
